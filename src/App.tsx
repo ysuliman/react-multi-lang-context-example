286bbbcf59e2cc4c8443cd756ef6af9ddad2caf9
Yousef Suliman <yousef.f.suliman@gmail.com>
2021-02-27 08:02:38 +0300

    Cleaned up template, added title, started made page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const content = {
+  en: {
+    title: 'React TS Multilang Context Example',
+    intro: 'This page was made as an example of how to use context in React to create a multilanguage website.'
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{content.en.title}</h1>
+      <p>{content.en.intro}</p>
     </div>
   );
 }
